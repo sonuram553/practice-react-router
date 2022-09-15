1c2c1d772680a01d103b2343e8dee18606b293e1
Sonu Ram <sonuram6960@gmail.com>
2022-09-15 23:24:44 +0530

    Create ErrorPage

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Root from "./routes/root";
 import "./index.css";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
